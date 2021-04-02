3079c46abe06f526fac36e4a0f66eb0137042726
Srdjan Obradovic <srdjan@3ap.ch>
2021-04-02 18:35:50 +0200

    Initial

---
+++ first-module/src/main/java/first/module/FirstModuleApplication.java
@@ -0,0 +1,9 @@
+package first.module;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstModuleApplication {
+    public static void main(String[] args) {
+    }
+}
---
+++ second-module/src/main/java/second/module/SecondModuleApplication.java
@@ -0,0 +1,10 @@
+package second.module;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondModuleApplication {
+    public static void main(String[] args) {
+
+    }
+}
