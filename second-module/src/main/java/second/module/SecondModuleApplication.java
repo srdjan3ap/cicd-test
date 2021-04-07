63adf4466dc240e303390b9a057255af1be0a6aa
Srdjan Obradovic <srdjan@3ap.ch>
2021-04-07 09:55:59 +0200

    changed some code

--- second-module/src/main/java/second/module/SecondModuleApplication.java
+++ second-module/src/main/java/second/module/SecondModuleApplication.java
@@ -5,6 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecondModuleApplication {
     public static void main(String[] args) {
-
+        System.out.println("hello cicd");
     }
 }
